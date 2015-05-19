fcc61ce0216bcf51d8c03b7dcb4dc8590d5c00a0
themostrichking <themostrichking@gmail.com>
2015-05-19 16:28:30 +0300

    new Human

---
+++ src/Human.java
@@ -0,0 +1,10 @@
+public abstract class Human {
+  private int age;
+  private int growth;
+  private int weight;
+  private Human mother;
+  private Human father;
+  private List<Human> kids;
+  private String docName;
+  private String sex;
+}
